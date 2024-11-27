8a5cd94cf4e6c5d9732599e8648c9b50390a11b1
danie <daniel2003@outlook.dk>
2024-11-27 08:37:11 +0100

    Add project files.

---
+++ DAL/Mappings/AutoMapperProfiles.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using DAL.Models.Domain;
+using DAL.Models.DTO;
+
+namespace NZWalks.API.Mappings
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            // Users
+            CreateMap<UpdateUserRequestDto, User>().ReverseMap();
+            CreateMap<AddUserRequestDto, User>().ReverseMap();
+            CreateMap<User, UserDto>().ReverseMap();
+
+            // PostalCode
+            CreateMap<PostalCode, PostalCodeDto>().ReverseMap();
+
+            // Genre
+            CreateMap<Genre, GenreDto>().ReverseMap();
+
+            // Movie
+            CreateMap<Movie, MovieDto>().ReverseMap();
+        }
+    }
+}
